46a2dc074ac89d49e28d3a2b1c94a978fe577057
misogohan <soypasterice@gmail.com>
2021-04-20 20:14:53 +0900

    adopt the path alias

--- components/App/Layout.tsx
+++ components/App/Layout.tsx
@@ -1,7 +1,7 @@
 import type { FC }           from 'react';
 import { createGlobalStyle } from 'styled-components';
-import Header                from '../Header';
-import Footer                from '../Footer';
+import Header                from '~/components/Header';
+import Footer                from '~/components/Footer';
 
 const Layout: FC = ({ children }) => <div>
   <Header/>
