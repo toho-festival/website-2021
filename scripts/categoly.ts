a2f3af9ca496dd43ed54643c1a421df98f8f7c81
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-04-28 03:17:53 +0900

    prepare-content2
    コンテンツ２の準備です。

---
+++ scripts/categoly.ts
@@ -0,0 +1,8 @@
+export type Categoly = {
+  categoly:string,
+  comment:string,
+}
+
+export const categoly1:Categoly[] = [{categoly: "体験型",comment: "体験で刻み込む桐朋祭の思い出"}];
+export const categoly2:Categoly[] = [{categoly: "展示", comment:"個性の爆発"}];
+export const categoly3:Categoly[] = [{categoly:"音響", comment:"桐朋生の奏でる最高のMUSIC"}];
