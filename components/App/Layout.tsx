0acce24dfbfefddf261f11d04f212309c18b8c67
rickytask-droid <75212102+rickytask-droid@users.noreply.github.com>
2021-06-09 17:36:52 +0900

    add-Layout.tsx

---
+++ components/App/Layout.tsx
@@ -0,0 +1,43 @@
+import type { FC }           from 'react';
+import { createGlobalStyle } from 'styled-components';
+import Header                from '~/components/Header';
+import Footer                from '~/components/Footer';
+
+const Layout: FC = ({ children }) => <div>
+  <Header/>
+  <div className='page-root'>
+    { children }
+  </div>
+  <Footer/>
+  <GlobalStyle/>
+</div>;
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 16px;
+    @media screen and (min-width: 768px) {
+      font-size: 24px;
+    }
+  }
+
+  body {
+    background: linear-gradient(#d4fffd, #ffe4bf) no-repeat;
+    min-height: 100vh;
+  }
+
+  * {
+    margin: 0;
+  }
+
+  a {
+    color: unset;
+    text-decoration: none;
+  }
+
+  .page-root {
+    padding-top: 3rem;
+  }
+`;
+
+export default Layout;
+
