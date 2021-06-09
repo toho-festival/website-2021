eb6719e6857c8485ab4abf356caf68ec0141649e
rickytask-droid <75212102+rickytask-droid@users.noreply.github.com>
2021-06-09 19:48:21 +0900

    add-_app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { FC }       from 'react';
+import type { AppProps } from 'next/app';
+import Layout            from '~/components/App/Layout';
+
+const App: FC<AppProps> = ({ Component, pageProps }) =>
+  <Layout>
+    <Component { ...pageProps }/>
+  </Layout>;
+
+export default App;
+
