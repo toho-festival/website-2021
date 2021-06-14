88b65855711007142a832f00dd80dd261c7a7898
rickytask-droid <75212102+rickytask-droid@users.noreply.github.com>
2021-06-14 20:53:23 +0900

    change-maincontent

--- src/components/Main/mainContent.tsx
+++ src/components/Main/mainContent.tsx
@@ -27,14 +27,13 @@ type ProjectProps = {
 const MainContent: FC<ProjectProps> = (props) =>{
   return(
     <>
-      <Project>
-        <Band side={props.side}>
-          <Image src={process.env.basePath + '/images/band808080.png'} width={800} height={200} layout='responsive' />
-        </Band>
-        <Categoly side={props.side} right={props.right} left={props.left}><Image src={process.env.basePath + props.categoly} width={17} height={20} layout='responsive' alt={"pictute of" + props.name}/></Categoly><ProjectTitle side={props.side}>{ projectsTitle[props.num] }</ProjectTitle>
-        <ProjectComment marginLeft={props.marginLeft}>{ projectsComment[props.num] }</ProjectComment>
-      </Project>
-      
+      <Wrap><Anchor>
+        <Project>
+          <Categoly side={props.side} right={props.right} left={props.left}><div><Image src={process.env.basePath + props.categoly} width={20} height={20} layout='responsive' alt={"pictute of" + props.name}/></div></Categoly>
+          <ProjectTitle side={props.side}>{ projectsTitle[props.num] }</ProjectTitle>
+          <ProjectComment marginLeft={props.marginLeft}>{ projectsComment[props.num] }</ProjectComment>
+        </Project>
+      </Anchor></Wrap>
       <Reset/>
     </>
   );
@@ -45,6 +44,13 @@ const Categoly = styled.div<Side>`
   margin-right: ${ (props) => props.right }%;
   margin-left: ${ (props) => props.left }%;
   width: 25%;
+  div{
+    transition-duration: 0.3s;
+    :hover{
+      transform: scale(1.05);
+      transition-duration: 0.3s;
+    }
+  }
 `;
 
 const Project = styled.section`
@@ -53,16 +59,52 @@ const Project = styled.section`
   margin-bottom: -5%;
 `;
 
-
-const Band = styled.div<Side>`
-  z-index: -1;
-  position: absolute;
-  margin-top: 4%;
-  right: ${ (props) => props.side === 'right' ? 0 : null };
-  left: ${ (props) => props.side === 'right' ? null : 0 };
-  width:85%;
-  height:40%;
+const Wrap = styled.div`
+border-radius: 0.5rem;
+  background-image: -webkit-linear-gradient(
+    315deg,
+    #704308 0%,
+    #ffce08 37%,
+    #fefeb2 47%,
+    #fafad6 50%,
+    #fefeb2 53%,
+    #e1ce08 63%,
+    #704308 100%
+  );
+  background-image: linear-gradient(
+    135deg,
+    #704308 0%,
+    #ffce08 37%,
+    #fefeb2 47%,
+    #fafad6 50%,
+    #fefeb2 53%,
+    #e1ce08 63%,
+    #704308 100%
+  );
 `;
+
+const Anchor = styled.a`
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+  text-align: center;
+  vertical-align: middle;
+  text-decoration: none;
+  letter-spacing: 0.1em;
+  color: #212529;
+  border-radius: 0.5rem;
+  :hover{
+    text-shadow: 0 0 15px rgba(250, 250, 214, 0.5),
+    0 0 15px rgba(250, 250, 214, 0.5), 0 0 15px rgba(250, 250, 214, 0.5),
+    0 0 15px rgba(250, 250, 214, 0.5);
+  }
+`;
+
+
 
 const ProjectTitle = styled.div<Side>`
   position: absolute;
@@ -70,6 +112,17 @@ const ProjectTitle = styled.div<Side>`
   color: #FBB03B;
   margin-left: ${ (props) => props.side === 'right' ? 30 : 38 }%;
   opacity: .8;
+  background: -webkit-gradient(
+    linear,
+    left bottom,
+    left top,
+    from(#ffffdb),
+    to(#a16422)
+  );
+  background: -webkit-linear-gradient(bottom, #ffffdb, #a16422);
+  background: linear-gradient(to top, #ffffdb, #a16422);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
   @media screen and (max-width: 1230px) {
     font-size: calc(24 / 1230 * 100vw);
   }
