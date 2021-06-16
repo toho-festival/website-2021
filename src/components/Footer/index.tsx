b80065421085a8445920a945377fbedeae9c717e
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-06-16 17:55:36 +0900

    change-footer-layout
    footerのレイアウトを変更しました。

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,33 +5,48 @@ import { footerRoutes } from '~/src/scripts/footerRoutes';
 
 const Footer: FC = () => <>
   <FooterContent>
-    <Theme>TohoFes 70th</Theme>
-    {
+    <Theme>Toho Festival 70th</Theme>
+    <Author>製作/デザイン：桐朋高校２年プログラム委員会</Author>
+      <CopyRight>
+        &copy; 2021 Toho70th-committee built this page.
+      </CopyRight>
+    {/* {
       footerRoutes.map(
         (sitemap) => <Sitemap key={ sitemap.toString() }>{ sitemap }</Sitemap>,
       )
-    }
+    } */}
   </FooterContent>
 </>;
 
 
 const FooterContent = styled.footer`
-  background-color: #0f233c;
-  color: white;
+  background-color: white;
+  color: black;
   display: flex;
   flex-direction: column;
-
-  > nav {
+  /* > nav {
     margin: 3rem 4rem
-  }
+  } */
 `;
 
 const Theme = styled.h2`
-  margin-left:1rem;
-  /* font-weight: bold; */
+  margin-top: 2%;
+  text-align: center;
+`;
+
+const Author = styled.small`
+  margin-top: 2%;
+  text-align: center;
+  font-size: small;
 `;
 
 const Sitemap = styled.nav`
+`;
+
+const CopyRight = styled.small`
+  text-align: center;
+  font-size: small;
+  margin-bottom: 1%;
 `;
 export default Footer;
 
