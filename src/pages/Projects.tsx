8dd91fe689ab017e34d97c9c0065ec4d9468b78b
rickytask <sagawa.riki.official@gmail.com>
2021-07-27 19:21:44 +0900

    add-pages/Projects.tsx
    commonLayoutの表示

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,8 @@
+import type { FC }   from 'react';
+import CommonLayout from '~/src/components/Projects/commonLayout/index';
+
+const Projects:FC = () =><>
+    <CommonLayout />
+</>
+
+export default Projects;
