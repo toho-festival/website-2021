2f3ed29d67e21d0bf8ab567b3a1d7c4978c3debf
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-07-29 17:51:38 +0900

    change-tagName
    タグ名を変更しました。

---
+++ src/components/Projects/Stage/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+import Definition from './definition';
+import Events from './events'
+
+
+const Stage:FC<{title: string}> = ({title}) => 
+  <>
+    <H1>{title}</H1>
+    <Definition />
+    <Events />
+  </>
+
+export default Stage;
+
+const H1 = styled.h1`
+  //a
+`;
