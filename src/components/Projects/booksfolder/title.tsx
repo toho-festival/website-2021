d663765455d73de557720a778b6df2578d054057
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-07-29 17:38:51 +0900

    cange-tagName
    タグ名を変更しました。

--- src/components/Projects/booksfolder/title.tsx
+++ src/components/Projects/booksfolder/title.tsx
@@ -1,29 +1,29 @@
 import { FC } from 'react';
 import styled from 'styled-components';
 
-
-
-const Title:FC = () => (
+const Title:FC = () => 
   <Wrap>
-    <H3>桐朋中学・高等学校</H3>
-    <H1>図書委員会</H1>
-    <H2>於　〇〇〇</H2>
+    <SchoolName>桐朋中学・高等学校</SchoolName>
+    <Name>図書委員会</Name>
+    <Location>於　〇〇〇</Location>
   </Wrap>
-)
+
 export default Title;
 
 const Wrap = styled.div`
 
 `;
-const H3 = styled.h3`
+const SchoolName = styled.h3`
     font-size: 3vw;
     text-align:center;
 `;
-const H1 = styled.h1`
+
+const Name = styled.h1`
     font-size:10vw;
     text-align:center;
 `;
-const H2 = styled.h2`
+
+const Location = styled.h2`
     color:red;
     font-size:6vw;
     text-align:center;
