4ef335c3d1916b6b9f68eaf41eaf9ed2c04f0ee3
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-07-29 17:38:10 +0900

    change-tagName
    タグ名を変更しました。

--- src/components/Projects/booksfolder/greeting.tsx
+++ src/components/Projects/booksfolder/greeting.tsx
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 
 
 
-const Greeting:FC = () => (
+const Greeting:FC = () => 
   <Wrap>
-    <H2>企画部長より一言</H2>
-    <H3>多くの高一の部員を桐朋祭委員に奪われ、続投するメン<br/>バーが二人という状況から始まった文化祭。この執筆時点<br/>（三月九日）では何が起こるかわかりません。ここに書いてあること<br/>は全て予定です。</H3>
+    <Title>企画部長より一言</Title>
+    <Greetings>多くの高一の部員を桐朋祭委員に奪われ、続投するメンバーが二人という状況から始まった文化祭。この執筆時点（三月九日）では何が起こるかわかりません。ここに書いてあることは全て予定です。</Greetings>
   </Wrap>
-)
+
 export default Greeting; 
 
 const Wrap = styled.div`
@@ -16,11 +16,11 @@ const Wrap = styled.div`
     margin-bottom: 8vw;
 `;
 
-const H2 = styled.h2`
+const Title = styled.h2`
     font-size:4vw;
     margin-left:5vw;
 `;
-const H3 = styled.h3`
+const Greetings = styled.h3`
     font-size:2vw;
     margin-left:7vw;
 `;
