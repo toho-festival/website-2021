3c514e989920635f4cf28ff699fe39390c1e6cfc
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-07-29 17:51:17 +0900

    change-tagName
    タグ名を変更しました。

--- src/components/Projects/Stage/definition.tsx
+++ src/components/Projects/Stage/definition.tsx
@@ -1,17 +1,26 @@
 import { FC } from 'react';
 import styled from 'styled-components';
 
+const Definition:FC = () => {
 
+  return(  
+    <>
+      <Title>ステージ企画とは？</Title>
+      <Discription>かくかくしかじか</Discription>
+      <Discription>かくかくしかじか</Discription>
+      <Discription>かくかくしかじか四角いムーブ</Discription>
+    </>
+  );
+}
 
-const Definition:FC = () => (
-  <>
-    <h3>ステージ企画とは？</h3>
-    <h4>かくかくしかじか</h4>
-
-    <h4>かくかくしかじか</h4>
-
-    <h4>かくかくしかじか四角いムーブ</h4>
-
-  </>
-)
 export default Definition;
+
+const Title = styled.h1`
+
+`;
+
+//このしたのpは怪しいから確認してね
+//確認したらこのコメントは消してください
+const Discription = styled.p`
+
+`;
