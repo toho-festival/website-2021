5fe35de6b90a4c396044cdc996f91b82c4e51646
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-07-29 17:50:03 +0900

    change-tagName
    タグ名を変更しました。

--- src/components/Projects/meisfolder/remarkable.tsx
+++ src/components/Projects/meisfolder/remarkable.tsx
@@ -3,50 +3,58 @@ import styled from 'styled-components';
 
 
 
-const Remarkable:FC = () => (
+const Remarkable:FC = () => 
   <>
-    <Div>
-        <Article>
-            <H2>見どころ</H2>
-            <H3>ただの迷路じゃない！<br /> 様々なトラップもあるかも！？</H3>
-            <Div>
-                <Img src="entrance-backgrund-1.jpg" alt="" />
-                <Img src="entrance-backgrund-1.jpg" alt="" />
-            </Div>
-        </Article>
-        <aside>
-            <H2content>企画内容</H2content>
-            <h3>からくり仕掛けの要素がたっぷり詰め込まれた迷路！！</h3>
-        </aside>
-    </Div>
+    <Wrap>
+      <Article>
+        <SubTitle>見どころ</SubTitle>
+        <Discription>ただの迷路じゃない！様々なトラップもあるかも！？</Discription>
+        <ImgWrap>
+            <Img src="entrance-backgrund-1.jpg" alt="" />
+            <Img src="entrance-backgrund-1.jpg" alt="" />
+        </ImgWrap>
+      </Article>
+      <Content>
+        <ContentTitle>企画内容</ContentTitle>
+        <Discription>からくり仕掛けの要素がたっぷり詰め込まれた迷路！！</Discription>
+      </Content>
+    </Wrap>
   </>
-)
+  
 export default Remarkable; 
 
 const Article = styled.article`
-    background-color:#ccffff;
+  background-color:#ccffff;
 `;
 
-const H2 = styled.h2`
-    margin-bottom:2vw;
-    border-bottom: 3px solid #0066cc;
+const SubTitle = styled.h2`
+  margin-bottom:2vw;
+  border-bottom: 3px solid #0066cc;
+  text-align: center
+`;
+
+const Content = styled.aside`
+
+`;
+ 
+const ContentTitle = styled.h2`
     text-align: center
 `;
 
-const H2content = styled.h2`
-    text-align: center
-`;
-
-const Div = styled.div`
-    display:flex;
-    gap : 1vw;
-    padding: 0 1vw 1vw 1vw;
+const Wrap = styled.div`
+  display:flex;
+  gap : 1vw;
+  padding: 0 1vw 1vw 1vw;
 `;
 
 const Img = styled.img`
-    width: 20vw;
+  width: 20vw;
 `;
 
-const H3 = styled.h3`
-    text-align: center
+const Discription = styled.h3`
+  text-align: center
+`;
+
+const ImgWrap = styled.div`
+
 `;
