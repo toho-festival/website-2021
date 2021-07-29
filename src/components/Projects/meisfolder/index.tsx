4bc837dbfb99b4b6f2ebfc850b3828a4e6c33e54
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-07-29 17:49:43 +0900

    change-path
    pathが間違っていたので変更しました。

--- src/components/Projects/meisfolder/index.tsx
+++ src/components/Projects/meisfolder/index.tsx
@@ -1,10 +1,9 @@
 import { FC } from 'react';
 import styled from 'styled-components';
 
-import Title from './meisfolder/title';
-import Remarkable from './meisfolder/remarkable';
-import Greeting from './meisfolder/greeting';
-
+import Title from './title';
+import Remarkable from './remarkable';
+import Greeting from './greeting';
 
 const Meis:FC = () => <Wrap>
     <Title />
