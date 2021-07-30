39cdd53e5410aadd3093c31d8a5ce651bf0ce478
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-07-31 00:24:02 +0900

    add-discription
    説明部分などの詳細な部分を追加しました。

--- src/components/FieldGame/index.tsx
+++ src/components/FieldGame/index.tsx
@@ -4,7 +4,6 @@ import { allProjects } from '~/src/scripts/all-projects';
 import { PiePlot } from './data';
 import { firebase } from '~/firebase/client';
 import { useCollection } from 'react-firebase-hooks/firestore';
-import { fileURLToPath } from 'url';
 
 const FieldGame: FC = () => {
 
@@ -17,8 +16,24 @@ const FieldGame: FC = () => {
       <Title>総務企画　〜陣地取りゲーム〜</Title>
       <Rules>
         <RuleTitle>ルール説明</RuleTitle>
-        <Explain>~~~~~~</Explain>
+        <Explain>
+          <Paragraph>各参加団体で最も来場者の割合が大きかったグループがその陣地(団体)を獲得することができます。</Paragraph>
+          <Paragraph>最終的に獲得した陣地の数が最も多いグループの勝利となります。</Paragraph>
+          <StepNumber>STEPⅠ</StepNumber>
+          <Content>ホームページ、入場時の案内で自分のグループの色を確認します 。</Content>
+          <StepNumber>STEPⅡ</StepNumber>
+          <Content>
+            <Paragraph>各参加団体の出口にある台紙に自分のグループの色のペンでマークして下さい。これを訪れた全ての参加団体で行います。</Paragraph>
+            <Paragraph>途中結果を確認するには…</Paragraph>
+            <Paragraph>① 昇降口での結果速報</Paragraph>
+            <Paragraph>② 第70回桐朋祭ホームページでの結果速報 </Paragraph>
+            <Paragraph>最終的に取得した陣地の数が最も多いグループの勝利となります</Paragraph>
+            <Paragraph>＜補足＞</Paragraph>
+            <Paragraph>結果の更新頻度は1時間を予定しています。</Paragraph>
+          </Content>
+        </Explain>
       </Rules>
+      <DataTitle>参加団体ごとの団体割合</DataTitle>
       <Data>
         {
           field?.docs.map(( doc ) => 
@@ -38,6 +53,7 @@ const FieldGame: FC = () => {
 
 const Title = styled.h1`
   color: white;
+  text-align: center;
 `;
 
 const Rules = styled.div``;
@@ -50,16 +66,28 @@ const Explain = styled.div`
   color: white;
 `;
 
-const Data = styled.div``;
+const StepNumber = styled.h2`
+
+`;
+
+const Content = styled.div`
+
+`;
+
+const Paragraph = styled.p`
+
+`;
+
+const DataTitle = styled.h2`
+  color: white;
+  text-align: center;
+`;
+
+const Data = styled.div`
+  margin-bottom: 10vw;
+  height: 29%;
+`;
 
 const Wrap = styled.div``;
-
-const DataTitle = styled.h3``;
-
-const RateBar = styled.div`
-  width:20%;
-  height:20%;
-  
-`;
 
 export default FieldGame;
