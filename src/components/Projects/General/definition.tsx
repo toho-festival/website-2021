8d70ade14a80e76b7ce7d937811886a624e22831
rickytask <sagawa.riki.official@gmail.com>
2021-08-01 02:53:28 +0900

    change-definition.tsx
    一般参加団体の説明部分のレイアウト変更

--- src/components/Projects/General/definition.tsx
+++ src/components/Projects/General/definition.tsx
@@ -3,23 +3,42 @@ import styled from 'styled-components';
 
 const Definition:FC = () => {
   return(  
-    <>
-      <Title>一般参加団体とは？</Title>
-      <Discription>かくかくしかじか</Discription>
-      <Discription>かくかくしかじか</Discription>
-      <Discription>かくかくしかじか四角いムーブ</Discription>
-    </>
+    <Wrap>
+      <Center><Title>一般参加団体とは？</Title></Center>
+      <Arrangement>
+        <Discription>{/*ここに文字を入れてはいけない*/}</Discription>
+        <Discription>かくかくしかじか</Discription>
+        <Discription>かくかくしかじか</Discription>
+        <Discription>かくかくしかじか四角いムーブ</Discription>
+      </Arrangement>
+    </Wrap>
   )
 }
 
 export default Definition;
 
-const Title = styled.h1`
-
+const Wrap = styled.div`
+  margin-top: 10vw;
+  margin-bottom: 20vw;
+  text-align: center;
 `;
 
-//このしたのpは怪しいから確認してね
-//確認したらこのコメントは消してください
-const Discription = styled.p`
+const Center = styled.div`
+  text-align: center;
+`;
 
+const Title = styled.h1`
+  display: inline-block;
+  border-bottom: #bf9d6d 6px solid;
+`;
+
+const Arrangement = styled.div`
+  margin-top: 3vw;
+  display: inline-block;
+`;
+
+const Discription = styled.p`
+  text-align: center;
+  border-bottom: 1px #ddd solid;
+  padding: 10px;
 `;
