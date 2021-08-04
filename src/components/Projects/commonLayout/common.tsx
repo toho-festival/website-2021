603879cb1d9ed207ccce838ec142b05a907088c1
rickytask <sagawa.riki.official@gmail.com>
2021-08-05 03:29:35 +0900

    reLayout-common.tsx
    レスポンシブに対応＆ちょっと直した

--- src/components/Projects/commonLayout/common.tsx
+++ src/components/Projects/commonLayout/common.tsx
@@ -8,11 +8,14 @@ const Common:FC = () => <>
             <Image src="/images/newExperience.png" width={20} height={20} layout='responsive' alt="シンボルロゴ"/>
         </Symbol>
         <Text>
+          <Center>
             <Subject>企画の名前！！</Subject>
-            <Explain>説明！！！</Explain>
-            <Explain>こんな感じで行ごとに区切ると</Explain>
-            <Explain>ボーダーが映えていい感じに見やすく</Explain>
-            <Explain>なると思うです</Explain>
+          </Center>
+          <Explain></Explain>
+          <Explain>説明！！！</Explain>
+          <Explain>こんな感じで行ごとに区切ると</Explain>
+          <Explain>ボーダーが映えていい感じに見やすく</Explain>
+          <Explain>なると思います</Explain>
         </Text>
     </Flex>
 
@@ -31,19 +34,26 @@ const Common:FC = () => <>
 </>
 
 const Flex = styled.div`
-    background-color: black;
-    display: flex;
-    justify-content: space-between;
-    padding-top: 7vw;
-    padding-bottom: 10vw;
+  background-color: black;
+  display: flex;
+  justify-content: space-between;
+  padding-top: 7vw;
+  padding-bottom: 10vw;
+  @media screen and (max-width: 1230px) {
+    display: block;
+  }
 `;
 
 const SecondFlex = styled.div`
-    background-color: white;
-    display: flex;
-    justify-content: space-between;
-    padding-top: 2vw;
-    padding-bottom: 10vw;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  padding-top: 2vw;
+  padding-bottom: 10vw;
+  @media screen and (max-width: 1230px) {
+    display: block;
+  }
+  border-bottom: 5px solid #000;
 `;
 
 const Symbol = styled.article`
@@ -58,19 +68,34 @@ const Picture = styled.article`
 `;
 
 const Text = styled.aside`
-    width: 60%;
+  margin-top: 5vw;
+  width: 60%;
+  @media screen and (max-width: 1230px) {
+    width: 100%;
+  }
 `;
 
 const MoreText = styled.aside`
-    margin-left: 2%;
-    width: 50%;
-    order: 1;
+  margin-left: 2%;
+  width: 50%;
+  order: 1;
+  @media screen and (max-width: 1230px) {
+    margin-left: 0%;
+    width: 100%;
+  }
+`;
+
+const Center = styled.div`
+  text-align: center;
 `;
 
 const Subject = styled.h1`
-    border-bottom: 5px white solid;
-    color: white;
-    display: inline-block;
+  border-bottom: 10px solid #bf9d6d;
+  color: white;
+  display: inline-block;
+  @media screen and (max-width: 1230px) {
+    border-bottom: 7px solid #bf9d6d;
+  }
 `;
 
 const MoreSubject = styled.h1`
@@ -80,10 +105,12 @@ const MoreSubject = styled.h1`
 `;
 
 const Explain = styled.h3`
-    border-bottom: 1px white solid;
-    color: white;
-    margin-left: 5%;
-    padding: 10px;
+  border-bottom: 1px white solid;
+  color: white;
+  padding: 10px;
+  @media screen and (max-width: 1230px) {
+    font-size: 12px;
+  }
 `;
 
 const MoreExplain = styled.h3`
