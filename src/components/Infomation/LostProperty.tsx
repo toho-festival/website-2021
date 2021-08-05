9a99fc60d53489eda30ff50431971c21846d5085
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-05 16:18:06 +0900

    change-layout-LostProperty
    lostPropertyのレイアウトを変更しました。

--- src/components/Infomation/LostProperty.tsx
+++ src/components/Infomation/LostProperty.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const LostProperty: FC = () => {
   return (
     <Wrap>
-      <Title>落とし物について</Title>
+      <Center><Title>落とし物について</Title></Center>
       <Pragraph>落とし物は総合案内所にて保管しております。お心当たりの方は総合案内所までお越し下さい。</Pragraph>
     </Wrap>
   );
@@ -13,13 +13,25 @@ const LostProperty: FC = () => {
 export default LostProperty;
 
 const Wrap = styled.div`
+  margin-bottom: 5vw;
+`;
 
+const Center = styled.div`
+  text-align: center;
 `;
 
 const Title = styled.h1`
-
+  margin-top: 4vw;
+  margin-bottom: 2vw;
+  font-size: 50px;
+  display: inline-block;
+  border-bottom: 10px solid #bf9d6d;
+  @media screen and (max-width: 1230px) {
+    font-size: 25px;
+    border-bottom: 7px solid #bf9d6d;
+  }
 `;
 
 const Pragraph = styled.p`
-
+  text-align: center;
 `;
