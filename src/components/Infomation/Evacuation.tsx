ee0cf7b220c2f5383e1bafe49db16720f07d556e
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-05 16:16:43 +0900

    change-layout-evacution
    evacutionのレイアウトを変更しました。

--- src/components/Infomation/Evacuation.tsx
+++ src/components/Infomation/Evacuation.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const Evacuation: FC = () => {
   return (
     <Wrap>
-      <Title>避難について</Title>
+      <Center><Title>避難について</Title></Center>
       <Pragraph>避難に関しては基本的に教職員の指示に従って、迅速に避難していただくようにお願い致します。避難場所は東グラウンドです。</Pragraph>
     </Wrap>
   );
@@ -16,10 +16,22 @@ const Wrap = styled.div`
 
 `;
 
-const Title = styled.h1`
+const Center = styled.div`
+  text-align: center;
+`;
 
+const Title = styled.h1`
+  margin-bottom: 2vw;
+  margin-top: 4vw;
+  font-size: 50px;
+  display: inline-block;
+  border-bottom: 10px solid #bf9d6d;
+  @media screen and (max-width: 1230px) {
+    font-size: 25px;
+    border-bottom: 7px solid #bf9d6d;
+  }
 `;
 
 const Pragraph = styled.p`
-
+  text-align: center;
 `;
