0203dfd601b67a573ed9b9b35ec1e45682c735fe
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-05 16:17:29 +0900

    change-layout-Lostchild
    lostChildのレイアウトを変更しました。

--- src/components/Infomation/LostChild.tsx
+++ src/components/Infomation/LostChild.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const LostChild: FC = () => {
   return (
     <Wrap>
-      <Title>迷子について</Title>
+      <Center><Title>迷子について</Title></Center>
       <Pragraph>
         迷子については、総合案内所で対応させて頂いております。
         迷子を発見された方、お連れの方とはぐれてしまった方は総合案内所までお越し下さい。
@@ -20,10 +20,21 @@ const Wrap = styled.div`
 
 `;
 
-const Title = styled.h1`
-
+const Center = styled.div`
+  text-align: center;
 `;
 
+const Title = styled.h1`
+  margin-bottom: 2vw;
+  margin-top: 4vw;
+  font-size: 50px;
+  display: inline-block;
+  border-bottom: 10px solid #bf9d6d;
+  @media screen and (max-width: 1230px) {
+    font-size: 25px;
+    border-bottom: 7px solid #bf9d6d;
+  }
+`;
 const Pragraph = styled.p`
-
+  text-align: center;
 `;
