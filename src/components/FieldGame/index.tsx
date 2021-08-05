6931ab440c56fb88319b25c2a0d1fe367a784cd1
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-05 11:49:36 +0900

    chage-Layout2
    ２度目のレイアウト変更を行いました。

--- src/components/FieldGame/index.tsx
+++ src/components/FieldGame/index.tsx
@@ -12,28 +12,49 @@ const FieldGame: FC = () => {
   )
   
   return(
-    <>
-      <Title>総務企画　〜陣地取りゲーム〜</Title>
+    <Wrap>
+      <Center>
+        <Title>総務企画　〜陣地取りゲーム〜</Title>
+      </Center>
       <Rules>
-        <RuleTitle>ルール説明</RuleTitle>
+        <Center>
+          <RuleTitle>ルール説明</RuleTitle>
+        </Center>
         <Explain>
-          <Paragraph>各参加団体で最も来場者の割合が大きかったグループがその陣地(団体)を獲得することができます。</Paragraph>
-          <Paragraph>最終的に獲得した陣地の数が最も多いグループの勝利となります。</Paragraph>
-          <StepNumber>STEPⅠ</StepNumber>
-          <Content>ホームページ、入場時の案内で自分のグループの色を確認します 。</Content>
-          <StepNumber>STEPⅡ</StepNumber>
-          <Content>
-            <Paragraph>各参加団体の出口にある台紙に自分のグループの色のペンでマークして下さい。これを訪れた全ての参加団体で行います。</Paragraph>
-            <Paragraph>途中結果を確認するには…</Paragraph>
-            <Paragraph>① 昇降口での結果速報</Paragraph>
-            <Paragraph>② 第70回桐朋祭ホームページでの結果速報 </Paragraph>
-            <Paragraph>最終的に取得した陣地の数が最も多いグループの勝利となります</Paragraph>
-            <Paragraph>＜補足＞</Paragraph>
-            <Paragraph>結果の更新頻度は1時間を予定しています。</Paragraph>
-          </Content>
+          <RuleParagraph>各参加団体で最も<Strong>来場者の割合</Strong>が大きかったグループがその陣地(団体)を獲得することができます。</RuleParagraph>
+          <RuleParagraph>最終的に獲得した陣地の数が最も多いグループの勝利となります。</RuleParagraph>
+          <RuleParagraph>グループは桐朋生、保護者の方々、外部のお客様の三種類です。</RuleParagraph>
+          <Flex>
+            <Border>
+              <Center>
+                <StepNumber>STEP<Color color="blue">Ⅰ</Color></StepNumber>
+              </Center>
+              <Content>ホームページ、入場時の案内で自分のグループの色を確認します 。</Content>
+              <Center><Image  src={process.env.basePath + "/images/airport-counter.png"}/></Center>
+            </Border>
+            <Border>
+              <Center><StepNumber>STEP<Color color="red">Ⅱ</Color></StepNumber></Center>
+              <Content>各参加団体の出口にある台紙に自分のグループの色のペンでマークして下さい。これを訪れた全ての参加団体で行います。</Content>
+              <Center><Image  src={process.env.basePath + "/images/writing-human.png"}/></Center>
+            </Border>
+          </Flex>
+          <Center>
+            <MoreInfo>
+              <ParagraphTitle>途中結果を確認するには…</ParagraphTitle>
+              <Frame>
+                <Paragraph>① 昇降口での結果速報</Paragraph>
+                <Paragraph>② 第70回桐朋祭ホームページでの結果速報 </Paragraph>
+                <Paragraph>最終的に取得した陣地の数が最も多いグループの勝利となります</Paragraph>
+                <Paragraph>＜補足＞</Paragraph>
+                <Paragraph>結果の更新頻度は1時間を予定しています。</Paragraph>
+              </Frame>
+            </MoreInfo>
+          </Center>
         </Explain>
       </Rules>
-      <DataTitle>参加団体ごとの団体割合</DataTitle>
+      <Center>
+        <DataTitle>参加団体ごとの団体割合</DataTitle>
+      </Center>
       <Data>
         {
           field?.docs.map(( doc ) => 
@@ -47,40 +68,128 @@ const FieldGame: FC = () => {
           )
         }
       </Data>
-    </>
+    </Wrap>
   )
 }
 
-const Title = styled.h1`
-  color: white;
+const Center = styled.div`
   text-align: center;
 `;
 
-const Rules = styled.div``;
+const Title = styled.h1`
+  margin-top: 3vw;
+  font-size: 50px;
+  color: black;
+  display: inline-block;
+  text-align: center;
+  border-bottom: 7px solid #bf9d6d;
+  @media screen and (max-width: 1230px) {
+    font-size: 25px;
+    border-bottom: 7px solid #bf9d6d;
+  }
+`;
+
+const Rules = styled.div`
+  margin-top: 10vw;
+`;
 
 const RuleTitle = styled.h2`
-  color: white;
+  margin-bottom: 2vw;
+  color: black;
+  display: inline-block;
+  text-align: center;
+  border-bottom: 4px solid #bf9d6d;
 `;
 
 const Explain = styled.div`
-  color: white;
+  color: black;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  margin-top: 5vw;
+  justify-content: space-between;
+`;
+
+const Border = styled.div`
+  width: 45%;
 `;
 
 const StepNumber = styled.h2`
+  display: inline-block;
+  text-align: center;
+  border-bottom: 4px solid #bf9d6d;
+`;
 
+const Color = styled.strong`
+  &[color='blue'] {
+        color: #3b82c4;
+  }
+  &[color='red'] {
+        color: #de4d4d;
+    }
 `;
 
 const Content = styled.div`
+  text-align: center;
+  margin-top: 1.5vw;
+  @media screen and (max-width: 1230px) {
+    font-size: 15px;
+  }
+`;
 
+const Image = styled.img`
+  margin-top: 2vw;
+  width: 80%;
+`;
+
+const RuleParagraph = styled.p`
+  margin-right: 10%;
+  margin-left: 10%;
+  text-align: center;
+  @media screen and (max-width: 1230px) {
+    font-size: 15px;
+  }
+`;
+
+const MoreInfo = styled.div`
+  margin-top: 5vw;
+  display: inline-block;
+`;
+
+const ParagraphTitle = styled.h2`
+  margin-bottom: 2vw;
+  border: 0px 0px 5px 2px;
+  border-style: none none solid dashed;
+  border-color: #bf9d6d;
+  display: inline-block;
+  text-align: initial;
+`;
+
+const Frame = styled.div`
+  border: dashed 2px #bf9d6d;
 `;
 
 const Paragraph = styled.p`
+  text-align: initial;
+  @media screen and (max-width: 1230px) {
+    font-size: 15px;
+  }
+`;
+
+const Strong = styled.strong`
 
 `;
 
 const DataTitle = styled.h2`
-  color: white;
-  text-align: center;
+  margin-top: 10vw;
+  margin-bottom: 3vw;
+  color: black;
+  display: inline-block;
+  border-bottom: 10px solid #bf9d6d;
+  @media screen and (max-width: 1230px) {
+    border-bottom: 7px solid #bf9d6d;
+  }
 `;
 
 const Data = styled.div`
@@ -88,6 +197,8 @@ const Data = styled.div`
   height: 29%;
 `;
 
-const Wrap = styled.div``;
+const Wrap = styled.div`
+  background-color: white;
+`;
 
 export default FieldGame;
