fc0d1967b6fdaabd59d2b06338165018251149c6
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-05 16:13:39 +0900

    change-layout-afterEvacution
    afterEvacutionのレイアウトを変更しました。

--- src/components/Infomation/AfterEvacution.tsx
+++ src/components/Infomation/AfterEvacution.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react";
 import styled from "styled-components";
 
-const AfterEvacution = () => {
+const AfterEvacution: FC = () => {
   return (
     <Wrap>
-      <Title>避難後について</Title>
+      <Center><Title>避難後について</Title></Center>
       <Pragraph>避難誘導の終了後は教職員が指示を出しますのでその場でお待ち下さい。</Pragraph>
     </Wrap>
   );
@@ -16,10 +16,22 @@ const Wrap = styled.div`
 
 `;
 
-const Title = styled.h1`
+const Center = styled.div`
+  text-align: center;
+`;
 
+const Title = styled.h1`
+  margin-bottom: 2vw;
+  margin-top: 4vw;
+  font-size: 50px;
+  display: inline-block;
+  border-bottom: 10px solid #bf9d6d;
+  @media screen and (max-width: 1230px) {
+    font-size: 25px;
+    border-bottom: 7px solid #bf9d6d;
+  }
 `;
 
 const Pragraph = styled.p`
-
+  text-align: center;
 `;
