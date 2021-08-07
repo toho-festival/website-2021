dda01ff4a747f05a141c75f79126773621c9b099
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-07 21:45:45 +0900

    delete-creater
    製作者の部分を削除しました。

--- src/components/Projects/meisfolder/greeting.tsx
+++ src/components/Projects/meisfolder/greeting.tsx
@@ -6,16 +6,11 @@ const Greeting:FC = () =>
     <Enthusiasm>
       昨年の中３学年参加委員が再び集結！！どんな方でも歓迎！ぜひ来てください！！
     </Enthusiasm>
-    <Creater>製作：</Creater>
   </>
 
 export default Greeting; 
 
 const Enthusiasm = styled.h2`
   margin-top:5vw;
-  text-align:center;
-`;
-
-const Creater = styled.h3`
   text-align:center;
 `;
