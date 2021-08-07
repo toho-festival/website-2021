8aa494c579bd82a1bb22d15d8c6bc12f4a95fe7b
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-07 21:46:27 +0900

    layout-booksClub
    図書委員の個別ページのレイアウトを変更しました。

--- src/components/Projects/booksfolder/title.tsx
+++ src/components/Projects/booksfolder/title.tsx
@@ -5,7 +5,7 @@ const Title:FC = () =>
   <Wrap>
     <SchoolName>桐朋中学・高等学校</SchoolName>
     <Name>図書委員会</Name>
-    <Location>於　〇〇〇</Location>
+    <Location>於　2-F</Location>
   </Wrap>
 
 export default Title;
@@ -13,22 +13,23 @@ export default Title;
 const Wrap = styled.div`
 
 `;
+
 const SchoolName = styled.h3`
-    font-size: 3vw;
-    text-align:center;
+  font-size: 3vw;
+  text-align:center;
 `;
 
 const Name = styled.h1`
-    font-size:10vw;
-    text-align:center;
+  font-size:10vw;
+  text-align:center;
 `;
 
 const Location = styled.h2`
-    color:red;
-    font-size:6vw;
-    text-align:center;
-    margin-top: 5vw;
-    :first-letter{
-        color:black;
-    }
+  color:red;
+  font-size:6vw;
+  text-align:center;
+  margin-top: 5vw;
+  :first-letter{
+    color:black;
+  }
 `;
