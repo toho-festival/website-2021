5b34800e56daa96021db91e76de142823538f9e8
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-10 13:49:31 +0900

    delete-extra-file
    いらなくなったファイルを削除します

--- src/pages/Projects.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC }   from 'react';
-import CommonLayout from '~/src/components/Projects/commonLayout/index';
-
-const Projects:FC = () =><>
-    <CommonLayout />
-</>
-
-export default Projects;
