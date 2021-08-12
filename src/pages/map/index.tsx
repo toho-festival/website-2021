8cc90cac96ff6ade52508dfea26651afbfd9aae5
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-12 23:17:21 +0900

    create-f1-page

---
+++ src/pages/map/index.tsx
@@ -0,0 +1,62 @@
+import { FC } from "react";
+import styled from "styled-components";
+import { getSortedPostsData } from '~/src/scripts/projects';
+import Link from 'next/link';
+
+type AllPropsDataType = {
+  id: string;
+  floor: string;
+  title: string;
+  building: string;
+}
+
+export const getStaticProps = async() =>{
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+const Map = ({ allPostsData }) => {
+  return (
+    <Wrap>
+      <MapImg src={process.env.basePath + "/images/f1.png"} />
+      <FloorProject>
+      {allPostsData.map(({ id, floor, title, building }:AllPropsDataType) => (
+          floor === "1" && building != "特別教室棟"? 
+          <Link href={"/projects/" + id}>
+            <a>
+              <ProjectImg src={process.env.basePath + "/images/photo-" + id + ".png"} />
+              <Project key={id}>{title}</Project>
+            </a>
+          </Link>
+          : null
+        ))
+      }
+      </FloorProject>
+    </Wrap>
+  );
+}
+
+export default Map;
+
+const Wrap = styled.div`
+`;
+
+const MapImg = styled.img`
+  width: 70%;
+`;
+
+const FloorProject = styled.div`
+
+`;
+
+const ProjectImg = styled.img`
+
+`;
+
+const Project = styled.div`
+
+`;
