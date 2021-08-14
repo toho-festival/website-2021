498b181ebae0d6c76f2d2b1ae7a0322aba5fb6ae
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-14 13:37:59 +0900

    fix-route

--- src/scripts/routes.ts
+++ src/scripts/routes.ts
@@ -8,7 +8,7 @@ export const routes: Route[] = [
   { logo: '/images/menu-newestInfo.jpg', key: '最新情報', path: '/news' },
   { logo: '/images/menu-holdingEvents.jpg', key: '開催中イベント', path: '/event' },
   { logo: '/images/menu-guide.jpg', key: 'ご案内', path: '/info' },
-  { logo: '/images/menu-map.jpg', key: 'マップ', path: '/maps' },
+  { logo: '/images/menu-map.jpg', key: 'マップ', path: '/map' },
   { logo: '/images/menu-aboutTohofes.jpg', key: '桐朋祭について', path: '/about' },
   { logo: '/images/menu-program.jpg', key: 'プログラム', path: '/program' },
   { logo: '/images/menu-covid19.jpg', key: 'コロナ対策', path: '/covid19' },
