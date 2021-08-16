90563f625fafbfc23c116470670bc501121626ad
misogohan <soypasterice@gmail.com>
2021-08-16 19:48:59 +0900

    開催中/開催予定の判定ロジックの最適化（そもろもクエリを変えろという話ですが、）

--- src/components/ProjectsEvent/index.tsx
+++ src/components/ProjectsEvent/index.tsx
@@ -1,139 +1,67 @@
-import { firestore } from "~/firebase/client";
-import styled from "styled-components";
-import { useCollection } from "react-firebase-hooks/firestore";
-import { useState, useEffect } from "react";
-import { notEnd, wasHeld, notStart } from "~/src/scripts/projectsEventIsHeld";
+import { useState, useEffect } from 'react';
+import styled                  from 'styled-components';
+import { useCollection }       from 'react-firebase-hooks/firestore';
+import { firestore }           from '~/firebase/client';
 
 const ProjectsEvent = () => {
-
-  const nowTime = new Date();
-  const nowMonth = nowTime.getMonth() + 1;
-  const nowDay = nowTime.getDate();
-  const nowHours = nowTime.getHours();
-  const nowMinutes = nowTime.getMinutes();
-  const nowSeconds = nowTime.getSeconds();
-  const nowAllSeconds = (nowMonth-8)*2678400+(nowDay-1)*86400+nowHours*3600+nowMinutes*60+nowSeconds;
-
-  const [dayCount, setDayCount] = useState(nowDay);
-  const [monthCount,setMonthCount] = useState(nowMonth);
-  const [hoursCount, setHoursCount] = useState(nowHours);
-  const [minutesCount, setMinutesCount] = useState(nowMinutes);
-  const [secondsCount, setSecondsCount] = useState(nowSeconds);
-  const [allSecondsCount, setAllSecondsCount] = useState(nowAllSeconds);
-
+  const [currentDate, setCurrentDate] = useState(new Date());
+  
   useEffect(() => {
-    
-    const id = setInterval(() => {
-      setAllSecondsCount(allSecondsCount+1)
-      if (secondsCount != 59 ) {
-        setSecondsCount(secondsCount + 1);
-      }else if (minutesCount != 59) {
-        setMinutesCount(minutesCount +1);
-        setSecondsCount(0);
-      }else if (hoursCount != 23) {
-        setHoursCount(hoursCount+1);
-        setMinutesCount(0);
-        setSecondsCount(0);
-      }else{
-        setDayCount(dayCount+1);
-        setHoursCount(0);
-        setMinutesCount(0);
-        setSecondsCount(0);
-      }
-    }, 1000);
+    const id = setInterval(() => setCurrentDate(new Date()), 100);
     return () => clearInterval(id);
-  }, [secondsCount,minutesCount,hoursCount,dayCount]);
-
-
-  const [event, eventLoading, eventError] = useCollection(
-    firestore.collection('project-event')
-  )
-
-  if(eventLoading) {
-    return(
-      <div>Loading...</div>
-    )
-  }
-
-  if(eventError) {
-    return(
-      <div>{`Error: ${eventError.message}`}</div>
-    )
-  }
-
-
-  return(
-  <>
+  }, []);
+  
+  const [event, eventLoading, eventError] = useCollection(firestore.collection('project-event'));
+  
+  if (eventLoading) return <div>Loading...</div>;
+  
+  if (eventError) return <div>{ `Error: ${ eventError.message }` }</div>;
+  
+  return <>
     <Center><Subject>開催中</Subject></Center>
     <Grid>
-      {event?.docs.map(doc => {
-        const startAt:Date = doc.data().duration.begin.toDate();
-        const startYear = startAt.getFullYear();
-        const startMonth = startAt.getMonth() + 1;
-        const startDay = startAt.getDate();
-        const startHours = startAt.getHours();
-        const startMinutes = startAt.getMinutes();
-        const startSeconds = startAt.getSeconds();
-
-        const endAt:Date = doc.data().duration.end.toDate();
-        const endYear = endAt.getFullYear();
-        const endMonth = endAt.getMonth() + 1;
-        const endDay = endAt.getDate();
-        const endHours = endAt.getHours();
-        const endMinutes = endAt.getMinutes();
-
-        return (
-        <Wrap>
-          { notEnd(endYear,endMonth,endDay,endHours,endMinutes,monthCount,dayCount,hoursCount,minutesCount) &&
-            wasHeld(startYear,startMonth,startDay,startHours,startMinutes,monthCount,dayCount,hoursCount,minutesCount)
-          ? 
+      { event!.docs.map(doc => {
+        const startAt: Date = doc.data().duration.begin.toDate();
+        const endAt: Date   = doc.data().duration.end.toDate();
+        
+        return <Wrap>
+          { !((+startAt < +currentDate) && (+currentDate < +endAt)) ? null :
             <Held>
-              <Title>{doc.data().title}</Title>
-              <Location>{doc.data().location.building}{doc.data().location.floor}階{doc.data().location.room}にて</Location>
+              <Title>{ doc.data().title }</Title>
+              <Location>{ doc.data().location.building }{ doc.data().location.floor }階{ doc.data().location.room }にて</Location>
             </Held>
-          : null}
-        </Wrap>
-        );
+          }
+        </Wrap>;
       })
       }
     </Grid>
-      <Center><Subject>開催予定</Subject></Center>
+    <Center><Subject>開催予定</Subject></Center>
     <Grid>
       {
-      event?.docs.map(doc => {
-        const startAt:Date = doc.data().duration.begin.toDate();
-        const startYear = startAt.getFullYear();
-        const startMonth = startAt.getMonth() + 1;
-        const startDay = startAt.getDate();
-        const startHours = startAt.getHours();
-        const startMinutes = startAt.getMinutes();
-        const startSeconds = startAt.getSeconds();
-        const byStartSeconds = (startMonth-8)*2678400+startDay*86400+startHours*3600+startMinutes*60+startSeconds;
-  
-        return (
-        <Wrap>
-          { notStart(startYear,startMonth,startDay,startHours,startMinutes,monthCount,dayCount,hoursCount,minutesCount) ? 
+        event!.docs.map(doc => {
+          const startAt: Date  = doc.data().duration.begin.toDate();
+          const byStartSeconds = Math.floor((+startAt - +currentDate) / 1000);
+          
+          return <Wrap>
+            { byStartSeconds <= 0 ? null :
               <NotHeld>
-                <Title>{doc.data().title}</Title>
+                <Title>{ doc.data().title }</Title>
                 <LeftTime>
-                  {Math.floor((byStartSeconds-nowAllSeconds)/86400)}日
-                  {Math.floor(((byStartSeconds-nowAllSeconds)%86400)/3600)}時間
-                  {Math.floor((((byStartSeconds-nowAllSeconds)%86400)%3600)/60)}分
-                  {(((byStartSeconds-nowAllSeconds)%86400)%3600)%60}秒</LeftTime>
-                <Location>{doc.data().location.building}{doc.data().location.floor}階{doc.data().location.room}にて</Location>
+                  { Math.floor(byStartSeconds / 86400) }日
+                  { Math.floor(byStartSeconds % 86400 / 3600) }時間
+                  { Math.floor(byStartSeconds % 86400 % 3600 / 60) }分
+                  { byStartSeconds % 86400 % 3600 % 60 }秒</LeftTime>
+                <Location>{ doc.data().location.building }{ doc.data().location.floor }階{ doc.data().location.room }にて</Location>
               </NotHeld>
-          : null }
-        </Wrap>
-        );
-      })
-    }
+            }
+          </Wrap>;
+        })
+      }
     </Grid>
-    </>
-  );
+  </>;
+};
 
-}
-
-export default ProjectsEvent
+export default ProjectsEvent;
 
 const Center = styled.div`
   text-align: center;
