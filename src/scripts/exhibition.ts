15fcecbb9f7d49089d20a73c9e3bc6731ceaee7f
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-24 00:27:40 +0900

    add kurineo

--- src/scripts/exhibition.ts
+++ src/scripts/exhibition.ts
@@ -5,5 +5,5 @@ export type project ={
 }
 
 export const exhibitionProject:project[] = [
-  {name:"生物部",id:"biology",url:"biologyClub2.jpg"},{name:"図書委員会",id:"books",url:"bookLogo.jpg"},{name:"てっけん！",id:"tekken",url:"tekkenLogo.jpg"},{name:"地学部",id:"geology",url:"geology1.jpg"},
+  {name:"生物部",id:"biology",url:"biologyClub2.jpg"},{name:"図書委員会",id:"books",url:"bookLogo.jpg"},{name:"てっけん！",id:"tekken",url:"tekkenLogo.jpg"},{name:"地学部",id:"geology",url:"geology1.jpg"},{name:"クリネオ",id:"kurineo",url:"kurineo.jpg"},
 ]
