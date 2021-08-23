f69d6cf367c7108136129df6c9d245cee19adafa
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-23 23:09:37 +0900

    add back button

--- src/pages/projects/books/title.tsx
+++ src/pages/projects/books/title.tsx
@@ -1,12 +1,18 @@
 import { FC } from 'react';
 import styled from 'styled-components';
+import { useRouter } from "next/router";
 
-const Title:FC = () => 
-  <Wrap>
-    <SchoolName>桐朋中学・高等学校</SchoolName>
-    <Name>図書委員会</Name>
-    <Location>於　2-F</Location>
-  </Wrap>
+const Title:FC = () => {
+  const router = useRouter();
+  return(
+    <Wrap>
+      <Button onClick={() => router.back()}>＜元のページに戻る</Button>
+      <SchoolName>桐朋中学・高等学校</SchoolName>
+      <Name>図書委員会</Name>
+      <Location>於　2-F</Location>
+    </Wrap>
+  )
+}
 
 export default Title;
 
@@ -31,5 +37,24 @@ const Location = styled.h2`
   margin-top: 5vw;
   :first-letter{
     color:black;
+  }
+`;
+
+const Button = styled.a`
+  border: 4px solid #ffffff;
+  margin-top: 4px;
+  border-radius: 150px;
+  background: #fff;
+  color: black;
+  -webkit-transform-style: preserve-3d;
+  transform-style: preserve-3d;
+  padding: 6px;
+
+  :hover {
+    color: #fff;
+    background: #1a1a1a;
+  }
+  @media screen and (max-width: 767px) {
+    padding: 2px;
   }
 `;
