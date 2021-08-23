da02efecce668e3e7c1ca3dc1ae9c404e6831bc0
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-23 15:26:40 +0900

    correct indent

--- src/components/Header/menu.tsx
+++ src/components/Header/menu.tsx
@@ -1,24 +1,28 @@
 import type { FC } from 'react';
-import Link        from 'next/link';
-import styled      from 'styled-components';
-import { routes }  from '~/src/scripts/routes';
+import Link from 'next/link';
+import styled from 'styled-components';
+import { routes } from '~/src/scripts/routes';
 
-const Menu: FC<{ display: boolean, toggle: () => void }> = ({ display, toggle }) => <Layout open={ display }>
+const Menu: FC<{ display: boolean, toggle: () => void }> = ({ display, toggle }) => <Layout open={display}>
   <Attention>※開発中のため、各ページに飛ぶことができません。ご了承ください。</Attention>
-  
-  
+
+
   <Box>
-  {
-    routes.map(
-      ({ key, logo, path }) =>
-      <Align>
-        <Flex>
-          <Image src={logo} alt="メニューのロゴ"/>
-          <span key={ key.toString() }><Link href={path}><a onClick={ toggle }><Anchor>{ key }</Anchor></a></Link></span>
-        </Flex>
-      </Align>
-    )
-  }
+    {
+      routes.map(
+        ({ key, logo, path }) =>
+          <Align>
+            <Link href={path}>
+              <a>
+                <Flex>
+                  <Image src={logo} alt="メニューのロゴ" />
+                  <span key={key.toString()}><a onClick={toggle}><Anchor>{key}</Anchor></a></span>
+                </Flex>
+              </a>
+            </Link>
+          </Align>
+      )
+    }
   </Box>
 </Layout>;
 
@@ -26,9 +30,6 @@ const Box = styled.div`
   margin-top: 3vw;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  @media screen and (max-width: 730px) {
-    grid-template-columns: 1fr 1fr;
-  }
 `;
 
 const Flex = styled.div`
@@ -50,6 +51,9 @@ const Image = styled.img`
 const Anchor = styled.p`
   font-size: 1.5rem;
   line-height: 3rem;
+  @media screen and (max-width: 730px) {
+    font-size: 0.8rem;
+  }
 `;
 
 
@@ -69,7 +73,7 @@ const Layout = styled.nav<{ open: boolean }>`
   padding-top: 1rem;
 
   background-color: rgba(0,0,0, 0.7);
-  transform: ${ ({ open }) => open ? 'translateY(0%)' : 'translateY(-100%)' };
+  transform: ${({ open }) => open ? 'translateY(0%)' : 'translateY(-100%)'};
   transition: transform 0.3s ease-in-out;
 `;
 
