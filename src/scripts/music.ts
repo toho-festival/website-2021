6ed192214708dc6f7a543f6e0d818da652393c70
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-23 18:14:40 +0900

    add-project

--- src/scripts/music.ts
+++ src/scripts/music.ts
@@ -5,5 +5,5 @@ export type project ={
 }
 
 export const musicProject:project[] = [
-  {name:"TAKE C",id:"take-c",url:""},{name:"MMT",id:"mmt",url:"mmtLogo.jpg"},{name:"THE NEETLES",id:"neetles",url:"neetlesLogo.jpg"},{name:"音楽部",id:"music",url:"musicClubLogo.jpg"},
+  {name:"TAKE C",id:"take-c",url:"acoustic-takec.png"},{name:"MMT",id:"mmt",url:"mmtLogo.jpg"},{name:"THE NEETLES",id:"neetles",url:"neetlesLogo.jpg"},{name:"音楽部",id:"music",url:"musicClubLogo.jpg"},
 ]
