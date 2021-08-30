4b0fd04d7a4a0e0559c7242c1cfccb0ccb094c2c
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-08-30 13:42:49 +0900

    add MainInformation page

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,14 @@
-import type { FC } from 'react';
-import styled      from 'styled-components';
-import Date        from '~/src/components/Main/date';
-import Project     from '~/src/components/Main/project';
-import SubContent  from '~/src/components/Main/sub-content';
+import type { FC }    from 'react';
+import styled         from 'styled-components';
+import Date           from '~/src/components/Main/date';
+import MainInfomation from '~/src/components/Main/main-infomation';
+import Project        from '~/src/components/Main/project';
+import SubContent     from '~/src/components/Main/sub-content';
 
 const Main: FC = () =>
   <Layout>
     <Date/>
+    <MainInfomation />
     <Project/>
     <SubContent/>
   </Layout>;
