64ec48e7a9a6daab58e1973f5746880c0f5c5126
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-09-01 23:41:34 +0900

    correct music club

--- src/scripts/music.ts
+++ src/scripts/music.ts
@@ -5,6 +5,6 @@ export type project ={
 }
 
 export const musicProject:project[] = [
-  {name:"TAKE C",id:"take-c",url:"acoustic-takec.png"},{name:"MMT",id:"mmt",url:"mmtLogo.jpg"},{name:"THE NEETLES",id:"neetles",url:"neetlesLogo.jpg"},{name:"音楽部",id:"music-club",url:"musicClubLogo.jpg"},
+  {name:"TAKE C",id:"take-c",url:"acoustic-takec.png"},{name:"MMT",id:"mmt",url:"mmtLogo.jpg"},{name:"THE NEETLES",id:"neetles",url:"neetlesLogo.jpg"},{name:"音楽部(今回は不参加)",id:"music-club",url:"musicClubLogo.jpg"},
   {name:"パラレルチンアナゴ",id:"parallel-music",url:"parallelMusicMember.jpg"},
 ]
