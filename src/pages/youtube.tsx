f36c3a515e2ed82ecfb90dd52c251a053c5b5828
Minizu424 <71531076+Minizu424@users.noreply.github.com>
2021-11-24 23:05:25 +0900

    add urls
    youtubeで公開した動画へのリンクを作成しました。

--- src/pages/youtube.tsx
+++ src/pages/youtube.tsx
@@ -1,9 +1,21 @@
 import { FC } from "react";
 import styled from "styled-components";
+import { YoutubeContents } from "~/src/scripts/youtube-contents";
 
 const Youtube = styled( props => <div { ...props }>
   <h1>桐朋祭の動画</h1>
-  <p><a href="">第70回桐朋祭の動画はここをクリック！</a></p>
+  {/* <p><a href="">第70回桐朋祭の動画はここをクリック！</a></p> */}
+  <table>
+  { YoutubeContents
+      .map(({name1, name2, id, url1, url2}) =>
+     
+        <tr key={ id }>
+          <td><a href={url1}>{name1}</a></td>
+          <td><a href={url2}>{name2}</a></td>
+        </tr>,
+      )
+    }
+  </table>
 </div>)`
   color: white;
 
@@ -12,17 +24,18 @@ const Youtube = styled( props => <div { ...props }>
     margin-top: 5%;
   }
 
-  > p{
-    text-align: center;
-    margin-top: 5%;
-    margin-bottom: 5%;
-
-    > a{
-      :hover{
-        color: #0bd;
+  > table{
+    margin: auto;
+    >tr {
+      > td{
+        text-align: center;
+        > a{
+          :hover{
+            color: #0bd;
+          }  
+        }  
       }
     }
-
   }
 `;
 
